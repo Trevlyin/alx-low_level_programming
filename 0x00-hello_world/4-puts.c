2de8a69d6359b2063cdc6a282c0beb40deb4ef56
Trevlyin <tmmuzinda@gmail.com>
2022-12-12 14:09:12 -0800

    puts with betty3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,14 @@
 
 /**
  * main - This function prints out string using puts
+ *
+ * Return: 0 for success
  */
 int main(void)
 {
   char str1[] = "\"Programming is like building a multilingual puzzle";
+
   puts(str1);
 
-  return(0);
+  return (0);
 }
