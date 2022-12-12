b073f20f50953119537a3e3905f7a2e43cb77c06
Trevlyin <tmmuzinda@gmail.com>
2022-12-12 14:00:17 -0800

    puts with betty2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+
 /**
-
  * main - This function prints out string using puts
-
  */
 int main(void)
 {
   char str1[] = "\"Programming is like building a multilingual puzzle";
   puts(str1);
+
   return(0);
 }
